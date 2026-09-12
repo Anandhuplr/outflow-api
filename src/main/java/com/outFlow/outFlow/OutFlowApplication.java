2813aee242aa974972ae8147b7c7d86019c60431
Anandhuplr <anandhukrishnan10@gamil.com>
2026-09-12 18:11:08 +0530

    Initial commit for project creation

---
+++ src/main/java/com/outFlow/outFlow/OutFlowApplication.java
@@ -0,0 +1,13 @@
+package com.outFlow.outFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutFlowApplication.class, args);
+	}
+
+}
